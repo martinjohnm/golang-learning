42654d5e2d0688ea439571edc53ffb267deb2e0a
martinjohnm <johnmmartin74@gmail.com>
2024-12-31 16:16:00 +0530

    linear story with next page addition method in struct

--- 7-linear-story/linear_story.go
+++ 7-linear-story/linear_story.go
@@ -2,32 +2,69 @@ package main
 
 import "fmt"
 
+// type storyPage struct {
+// 	text string
+// 	nextPage *storyPage
+// }
+
+
+// func playStory(page *storyPage) {
+
+// 	if page == nil { // checking for null pointer
+// 		return
+// 	}
+
+// 	fmt.Println(page.text)
+
+// 	playStory(page.nextPage)
+// }
+
 type storyPage struct {
 	text string
 	nextPage *storyPage
 }
 
 
-func playStory(page *storyPage) {
+func (page *storyPage) playStory() {
+	// //with recursion
+	// if page == nil {
+	// 	return
+	// }
 
-	if page == nil {
-		return
+	// fmt.Println(page.text)
+	// page.nextPage.playStory()
+
+	// with loop 
+	for page != nil {
+		fmt.Println(page.text)
+		page = page.nextPage
+	}
+}
+
+func (page *storyPage) addToEnd(text string) {
+
+	pagePointerToAdd := &storyPage{text, nil}
+
+	for page.nextPage != nil {
+		page = page.nextPage
 	}
 
-	fmt.Println(page.text)
+	page.nextPage = pagePointerToAdd
 
-	playStory(page.nextPage)
 }
+
 
 
 
 func main() {
 	page1 := storyPage{"1st page", nil}
-	page2 := storyPage{"2nd page", nil}
-	page3 := storyPage{"3rd page", nil}
+	page1.addToEnd("2nd Page")
+	page1.addToEnd("3rd page")
 
-	page1.nextPage = &page2
-	page2.nextPage = &page3
 
-	playStory(&page1)
+	page1.playStory() // playStory(&page1) // in the first approach
+
+	// Functions - has return value - 
+	// Procedures - has no return value just executes commands
+	// Methods - functions that are attached to struct/obj/etc
 }
